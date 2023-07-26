eed80082c97a9c288bb891adcdd4c6b831e65ca1
umanema <dn.antropov@gmail.com>
2023-07-26 14:11:28 +0200

    test install.sh on Ubuntu 22.04.2

--- auto-vton/install.sh
+++ auto-vton/install.sh
@@ -1,17 +1,41 @@
-cd ~/repositories
-git submodule update --init --recursive --remote
+#!/bin/bash
+USER_HOME=$(eval echo ~${SUDO_USER})
+cd $USER_HOME/repositories/ladi-vton-pipeline
+sudo git submodule update --init --recursive --remote
+
+cd $USER_HOME
 
 #install miniconda
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh
-bash ~/miniconda.sh -b -p $HOME/miniconda
-eval "$(~/miniconda3/bin/conda shell.bash hook)"
-conda init
+MINICONDA=$USER_HOME/miniconda.sh
+if [[ -f "$MINICONDA" ]]; then
+    echo "miniconda is already installed on the system"
+else
+    echo "installing miniconda" 
+    wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O $USER_HOME/miniconda.sh
+    sh $USER_HOME/miniconda.sh -b -p $USER_HOME/miniconda3 -u
+    eval "$($USER_HOME/miniconda3/bin/conda shell.bash hook)"
+fi
+source $USER_HOME/miniconda3/etc/profile.d/conda.sh
 
-conda create ladi-vton-pipeline
-conda activate ladi-vton-pipeline
+if { conda env list | grep ladi-vton-pipeline ; } >/dev/null 2>&1 ; then
+    echo "ladi-vton-anaconda already exists";
+    conda activate ladi-vton-pipeline
+else
+    echo "creating ladi-vton pipeline env"
+    conda create -n ladi-vton-pipeline python=3.10 -y
+    conda activate ladi-vton-pipeline
+fi
+
+
 
 #install carvekit before everything else
-pip install carvekit
+if python -c "import carvekit" &> /dev/null; then
+    echo 'carvekit is already installed'
+else
+    echo 'installing carvekit'
+    pip install --no-input carvekit
+fi
+
 
 #install cuda and cudnn
 sudo apt-get install linux-headers-$(nsynk -r)
@@ -20,67 +44,92 @@ wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/
 sudo dpkg -i cuda-keyring_1.1-1_all.deb
 sudo apt-get update
 
-sudo apt-get install cuda-11-8
+sudo apt-get -qq install --yes --no-upgrade cuda-11-8
 
 #install tensorflow
-conda install -c "nvidia/label/cuda-11.8.0" cuda-toolkit
-python -m pip install nvidia-cudnn-cu11==8.6.0.163 tensorflow==2.12.*
+conda install -y -c "nvidia/label/cuda-11.8.0" cuda-toolkit
+python -m pip install --no-input nvidia-cudnn-cu11==8.6.0.163 tensorflow==2.12.*
+echo $CONDA_PREFIX
 mkdir -p $CONDA_PREFIX/etc/conda/activate.d
 echo 'CUDNN_PATH=$(dirname $(python -c "import nvidia.cudnn;print(nvidia.cudnn.__file__)"))' >> $CONDA_PREFIX/etc/conda/activate.d/env_vars.sh
 echo 'export LD_LIBRARY_PATH=$CONDA_PREFIX/lib/:$CUDNN_PATH/lib:$LD_LIBRARY_PATH' >> $CONDA_PREFIX/etc/conda/activate.d/env_vars.sh
 source $CONDA_PREFIX/etc/conda/activate.d/env_vars.sh
 
-mkdir ~/repositories/ladi-vton-pipeline/auto-vton/input/cloth
-mkdir ~/repositories/ladi-vton-pipeline/auto-vton/input/person
-mkdir ~/repositories/ladi-vton-pipeline/auto-vton/resize/cloth
-mkdir ~/repositories/ladi-vton-pipeline/auto-vton/resize/person
-mkdir ~/repositories/ladi-vton-pipeline/auto-vton/result
+mkdir $USER_HOME/repositories/ladi-vton-pipeline/auto-vton/input/cloth
+mkdir $USER_HOME/repositories/ladi-vton-pipeline/auto-vton/input/person
+mkdir $USER_HOME/repositories/ladi-vton-pipeline/auto-vton/resize/cloth
+mkdir $USER_HOME/repositories/ladi-vton-pipeline/auto-vton/resize/person
+mkdir $USER_HOME/repositories/ladi-vton-pipeline/auto-vton/result
 
-mkdir ~/repositories/ladi-vton-pipeline/ladi-vton/data/hd-viton/test/agnostic-v3.2
-mkdir ~/repositories/ladi-vton-pipeline/ladi-vton/data/hd-viton/test/cloth
-mkdir ~/repositories/ladi-vton-pipeline/ladi-vton/data/hd-viton/test/cloth-mask
-mkdir ~/repositories/ladi-vton-pipeline/ladi-vton/data/hd-viton/test/image
-mkdir ~/repositories/ladi-vton-pipeline/ladi-vton/data/hd-viton/test/image-densepose
-mkdir ~/repositories/ladi-vton-pipeline/ladi-vton/data/hd-viton/test/image-parse-agnostic-v3.2
-mkdir ~/repositories/ladi-vton-pipeline/ladi-vton/data/hd-viton/test/image-parse-v3
-mkdir ~/repositories/ladi-vton-pipeline/ladi-vton/data/hd-viton/test/openpose_img
-mkdir ~/repositories/ladi-vton-pipeline/ladi-vton/data/hd-viton/test/openpose_json
+mkdir $USER_HOME/repositories/ladi-vton-pipeline/ladi-vton/data/hd-viton/test/agnostic-v3.2
+mkdir $USER_HOME/repositories/ladi-vton-pipeline/ladi-vton/data/hd-viton/test/cloth
+mkdir $USER_HOME/repositories/ladi-vton-pipeline/ladi-vton/data/hd-viton/test/cloth-mask
+mkdir $USER_HOME/repositories/ladi-vton-pipeline/ladi-vton/data/hd-viton/test/image
+mkdir $USER_HOME/repositories/ladi-vton-pipeline/ladi-vton/data/hd-viton/test/image-densepose
+mkdir $USER_HOME/repositories/ladi-vton-pipeline/ladi-vton/data/hd-viton/test/image-parse-agnostic-v3.2
+mkdir $USER_HOME/repositories/ladi-vton-pipeline/ladi-vton/data/hd-viton/test/image-parse-v3
+mkdir $USER_HOME/repositories/ladi-vton-pipeline/ladi-vton/data/hd-viton/test/openpose_img
+mkdir $USER_HOME/repositories/ladi-vton-pipeline/ladi-vton/data/hd-viton/test/openpose_json
 
 #openpose
-sudo apt-get -qq install -y libatlas-base-dev libprotobuf-dev libleveldb-dev libsnappy-dev libhdf5-serial-dev protobuf-compiler libgflags-dev libgoogle-glog-dev liblmdb-dev opencl-headers ocl-icd-opencl-dev libviennacl-dev libboost-all-dev
-
 sudo apt-get update
-sudo apt-get install cmake
-sudo apt-ge install libopencv-dev python3-opencv
-sudo apt-get install libprotobuf-dev protobuf-compiler
+sudo apt-get -qq install -y --no-upgrade libatlas-base-dev libprotobuf-dev libleveldb-dev libsnappy-dev libhdf5-serial-dev protobuf-compiler libgflags-dev libgoogle-glog-dev liblmdb-dev opencl-headers ocl-icd-opencl-dev libviennacl-dev libboost-all-dev libopencv-dev python3-opencv cmake
 
-cd ~/repositories/ladi-vton-pipeline/openpose/
-git submodule update --init --recursive --remote
+#anaconda might have issues with std libs so I link it to the one installed with apt
+ln -sf /usr/lib/x86_64-linux-gnu/libstdc++.so.6 $CONDA_PREFIX/lib/libstdc++.so.6
 
-mkdir build/
+cd $USER_HOME/repositories/ladi-vton-pipeline/openpose
+# git submodule update --init --recursive --remote
+
+BUILD=./build
+if [[ -f "$BUILD" ]]; then
+    mkdir build/
+fi
 cd build/
 
-cmake -D USE_CUDNN=ON -D CUDNN_LIBRARY="$CUDNN_PATH/lib" -D CUDNN_INCLUDE="$CUDNN_PATH/include"  -S ../ -B ./
-make -j`nproc`
+# # if grep -qi microsoft /proc/version; then
+# #   echo "Ubuntu on Windows"
+# #   conda install -y -c anaconda libffi=3.3
+# # else
+# #   echo "native Linux"
+# # fi
+BIN=./examples/openpose/openpose.bin
+if [[ -f "$BIN" ]]; then
+    echo "openpose is already built"
+else
+    echo "building openpose"
+    sudo cmake -D USE_CUDNN=ON -D CUDNN_LIBRARY="$CUDNN_PATH/lib" -D CUDNN_INCLUDE="$CUDNN_PATH/include"  -S ../ -B ./
+    sudo make -j`nproc`
+fi
 
 #humanparse
+pip install --no-input torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+pip install --no-input tf_slim matplotlib gdown
 
-pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
-pip install tf_slim
-pip install matplotlib
-pip install gdown
-sudo apt-get install unzip
-
-cd ~/repositories/CHIP_PGN
-gdown --id=1Mqpse5Gen4V4403wFEpv3w3JAsWw2uhk
-unzip CIHP_pgn.zip
-mv ./CIHP_pgn ./CIHP_PGN/checkpoint/
+sudo apt-get -qq install -y --no-upgrade unzip
+cd ~/repositories/ladi-vton-pipeline/CIHP_PGN
+CHECKPOINT=./checkpoint/CIHP_pgn
+if [[ -d "$CHECKPOINT" ]]; then
+    echo "checkpoint CIHP_pgn already exist"
+else
+    gdown --id "1Mqpse5Gen4V4403wFEpv3w3JAsWw2uhk"
+    unzip CIHP_pgn.zip
+    mkdir checkpoint/CIHP_pgn
+    mv ./CIHP_pgn/* ./checkpoint/CIHP_pgn/
+    rm ./CIHP_pgn.zip
+    rm -d ./CIHP_pgn
+fi
 
 #densepose
 cd ~/repositories/ladi-vton-pipeline
-python -m pip install -e detectron2
-python -m pip install av
-pip install accelerate
+if pip list | grep detectron2 &> /dev/null; then
+    echo "detectron2 is already installed"
+else
+    sudo $CONDA_PREFIX/bin/python -m pip install -e detectron2
+fi
+
+python -m pip install --no-input av
+pip install --no-input accelerate
 
 #ladi-vton
-pip install diffusers==0.14.0 transformers==4.27.3 accelerate==0.18.0 clean-fid==0.1.35 torchmetrics[image]==0.11.4 wandb==0.14.0 matplotlib==3.7.2 tqdm xformers
+pip install --no-input diffusers==0.14.0 transformers==4.27.3 accelerate==0.18.0 clean-fid==0.1.35 torchmetrics[image]==0.11.4 wandb==0.14.0 matplotlib==3.7.2 tqdm xformers
