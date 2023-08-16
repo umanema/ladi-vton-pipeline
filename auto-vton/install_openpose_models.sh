56459188967fdf81328dc3bdbdb5418ab15709e6
umanema <dn.antropov@gmail.com>
2023-08-16 10:08:48 +0200

    activate conda environment when installin g openpose models

--- auto-vton/install_openpose_models.sh
+++ auto-vton/install_openpose_models.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 USER_HOME=$(eval echo ~${SUDO_USER})
 
+source $USER_HOME/miniconda3/etc/profile.d/conda.sh
+conda activate ladi-vton-pipeline
+
 cd $USER_HOME/repositories/ladi-vton-pipeline/openpose
 
 #download openpose models from GDrive
